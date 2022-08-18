3129e69ac20d9547fc22930a8e251b7ecd6d0f4f
Mark Muyuela <mark.muyuela81@gmail.com>
2022-08-18 15:30:23 +0100

    syntax errors corrected

--- router.js
+++ router.js
@@ -1,26 +1,26 @@
-import topicController from "./controller/topicController.js";
-import express from "express";
+import topicController from './controllers/topicController.js'
+import commentController from './controllers/commentController.js'
+import userController from './controllers/userController.js'
+import express from 'express'
 
 const router = express.Router
 
-router.route("/").get((req, res) => res.status(200).send("API is running"));
+router.route('/').get((req, res) => res.status(200).send('API is running'))
 
 router
-    .route("/topic")
-    .get(topic.getAll)
-    .post(topicController.post)
+  .route('/topic')
+  .get(topicController.getAll)
+  .post(topicController.post)
 
 router
-    .route("/topic/:id")
-    .get(topicController.getIndividual)
-    .put(topicController.update)
-    .delete(topicController.remove);
+  .route('/topic/:id')
+  .get(topicController.getIndividual)
+  .put(topicController.update)
+  .delete(topicController.remove)
 
-router.route("/register").post(userController.register);
-router.route("/login").post(userController.login);
-router.route("/comment/:topicId").post(commentController.create)
-router.route("/comment/:topicId/:commentId")
-.put(commentController.update)
-.delete(commentController.remove);
+router.route('/register').post(userController.register)
+router.route('/login').post(userController.login)
+router.route('/comment/:topicId').post(commentController.create)
+router.route('/comment/:topicId/:commentId').put(commentController.update).delete(commentController.remove)
 
 export default router
