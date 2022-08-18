14cd13b911e563ade511514179090eae7d12cc2b
dr-veronica <veronica.deronzi@gmail.com>
2022-08-18 12:34:01 +0100

    logger

---
+++ middleware.js/logger.js
@@ -0,0 +1,7 @@
+const logger = (req, res, next) => {
+    console.log(`Incoming request: Method: ${req.method} - URL: ${req.url}`);
+
+    next();
+  };
+  
+  export default logger;
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import mongoose from 'mongoose'
 
 console.log('hello from express!')
 const PORT = 4000
@@ -10,21 +9,11 @@ const connectToDb = async () => {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
-  // URI connection string: unique identifier to point to a database
-  // in our case, we're connecting to a local database
-  // connection string is of pattern:
-  // DB_FLAVOUR://HOST:PORT/DATABASE_NAME
-  // DATABASE_NAME does not have to exist yet, mongoose will create it automatically.
-  // Host and port have to be right though! If no DB server is running there, we won't
-  // be able to connect.
-  return mongoose.connect('mongodb://localhost:27017/tapas', opts)
+  return mongoose.connect('mongodb://localhost:27017/topic', opts)
 }
 const startServer = async () => {
   await connectToDb()
   console.log('Database has connected successfully')
-  // app.listen(PORT, print out information when app is listening)
-  // after running app.listen(), we are going to be listening on any
-  // incoming requests onto our localhost:PORT
   app.listen(PORT, () => {
     console.log(`:rocket: Express server running on port ${PORT} :rocket:`)
   })
