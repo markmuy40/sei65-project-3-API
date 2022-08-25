1ae8770851b1b17abe2f1509abe8b9b15908f09f
rokster112 <rokster112@gmail.com>
2022-08-25 12:57:43 +0100

    second deployment

--- const.js
+++ const.js
@@ -6,9 +6,7 @@ const consts = {
   DB_CONNECTION_STRING:
     process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/topic',
   PORT: process.env.PORT || 4000,
-  // JWT_SECRET: process.env.JWT_SECRET || '12345',
-  // ADMIN_HASHPASSWORD: process.env.ADMIN_HASHPASSWORD || '12345',
-  // USER_HASHPASSWORD: process.env.USER_HASHPASSWORD || '12345',
+  JWT_SECRET: process.env.JWT_SECRET || 'JWT_SECRET',
 }
 console.log(consts)
 export default consts
